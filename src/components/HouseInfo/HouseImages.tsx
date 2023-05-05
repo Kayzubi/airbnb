18e3fb541b0bc97fa8d826643295c9eedbc987c5
Kizito Azubuike <kizito.azubuike1@gmail.com>
2023-05-05 13:31:26 +0100

    add responsive styles

--- src/components/HouseInfo/HouseImages.tsx
+++ src/components/HouseInfo/HouseImages.tsx
@@ -11,12 +11,13 @@ const HouseImages = ({ images }: Prop) => {
       <Col lg='7' md='8' className='house-details__image'>
         <div className='house-details__image--1'>
           <img src={images[0]} alt='' />
+          <span>View all photos</span>
         </div>
       </Col>
       <Col
         lg='5'
         md='4'
-        className='house-details__image d-flex flex-column justify-content-between gap-4'>
+        className='house-details__image d-flex flex-column justify-content-between gap-4 mobile-hidden'>
         <div className='house-details__image--2'>
           <img src={images[1]} alt='' />
         </div>
